1a76fcef39f660b8204cefa13310eaa22259a2eb
Parichat Saelao <177830086+Parichat0096@users.noreply.github.com>
2025-09-23 00:15:01 +0700

    edit week06

--- week06/starter/script.js
+++ week06/starter/script.js
@@ -1,25 +1,24 @@
 // Lesson 1: Arrays, Objects, and Functions
-
+ 
 // 1. Create an empty array to hold the quotes
 const quotes = []
-
-const quote = {id, content, author}
-
-
-
+ 
 /*
   2. Function: addQuote
-  - Accepts a quote object with id, content, and author
+  - Accepts a quote object with id, const outerFunction = (a) => {
+    const innerFunction = (b) => {
+        return a + b;
+    }
+}
+ 
+content, and author
   - Adds it to the quotes array
 */
 function addQuote(quote) {
   // TODO: Add the quote object to the quotes array
-  return quote.push(quotes)
+  quotes.push(quote);
 }
-
-
-
-
+ 
 /*
   3. Function: deleteQuote
   - Accepts an id
@@ -27,13 +26,12 @@ function addQuote(quote) {
 */
 function deleteQuote(id) {
   // TODO: Remove the quote object from the array using the given id
-  quotes = quotes.filter(quote => quote.id !== id)
-
+  const index = quotes.findIndex(q => q.id === id);
+  if (index !== -1) {
+    quotes.splice(index, 1);
+  }
 }
-
-
-
-
+ 
 /*
   4. Function: updateQuote
   - Accepts an id and an object with new content and/or author
@@ -41,28 +39,42 @@ function deleteQuote(id) {
 */
 function updateQuote(id, updatedQuote) {
   // TODO: Find the quote by id and update its properties
+  quote = quotes.find( q => quotes.id === id)
+  if (quote) {
+    if (updatedQuote.content !== undefined) {
+      quote.content = updatedQuote.content;
+    }
+    if (updatedQuote.author !== undefined) {
+      quote.author = updatedQuote.author;
+    }
+  }
 }
-
+ 
 /*
   5. Function: getAllQuotes
   - Returns all quotes in the array
 */
 function getAllQuotes() {
   // TODO: Return the quotes array
+  return quotes
 }
-
+ 
 // 6. Test your functions below
 // TODO: Add 3 quotes using addQuote()
-
+addQuote({ id: 1, content: "The only way to do great work is to love what you do.", author: "Steve Jobs" });
+addQuote({ id: 2, content: "Life is what happens when you're busy making other plans.", author: "John Lennon" });
+addQuote({ id: 3, content: "Get busy living or get busy dying.", author: "Stephen King" });
+console.log(getAllQuotes())
+console.log("---------------------------")
 // TODO: Delete 1 quote using deleteQuote()
-
+deleteQuote(2);
+console.log(getAllQuotes())
+console.log("---------------------------")
 // TODO: Update 1 quote using updateQuote()
-
+updateQuote(3, { content: "Either get busy living or get busy dying.", author: "Stephen King (Updated)" });
+console.log(getAllQuotes())
+console.log("---------------------------")
 // TODO: Print all quotes using getAllQuotes()
-
-
-
-
-
-
-
+console.log(getAllQuotes());
+console.log("---------------------------")
+ 
